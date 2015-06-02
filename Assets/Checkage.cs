ea8ae6d3e41bbafe77fe28699a8271b2022ca700
hbcasper <hb.casper@gmail.com>
2015-06-02 12:46:17 +0200

    New starting scenes and scripts

---
+++ Assets/Checkage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Checkage : MonoBehaviour {
+
+	Text introducedage;
+	Transform startbutton;
+
+	// Use this for initialization
+	void Start () {
+		introducedage = GetComponent <Text> ();
+		startbutton = GameObject.Find ("Startexperiment");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void checkage (){
+
+		if(introducedage.text == "1"){
+			startbutton.GetComponent<Button>().interactable = false; 
+		}
+
+}
