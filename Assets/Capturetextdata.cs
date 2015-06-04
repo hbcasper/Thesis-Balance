bcd3a2ef2065f83d99458c4e574c7f1a1a982706
hbcasper <hb.casper@gmail.com>
2015-06-04 12:14:25 +0200

    Capture participant number

---
+++ Assets/Capturetextdata.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Capturetextdata : MonoBehaviour {
+
+	Text participantNumberInput;
+	public string participantNumber;
+
+	// Use this for initialization
+	void Start () {
+
+
+		participantNumberInput = GetComponent <Text> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	public void SaveParticipantNumber () {
+
+		participantNumber = participantNumberInput.text;
+	
+	}
+}
--- Assets/Userfinaldata.cs
+++ Assets/Userfinaldata.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class Userfinaldata : MonoBehaviour {
 
-	public string participantAge;
+	public string participantAge; //Variable that save the age of the participant
 
 
-	public string participantGender;
+	public string participantGender; //Variable that save the gender of the participant
 
+	public string participantNumber; //Variable that save the number of the participant
 
 	GameObject participantageobject;
 	Checkage participantage;
+
+	GameObject participantNumberObject;
+	Capturetextdata participantNumberData;
 
 	void Start(){
 		
@@ -37,4 +41,13 @@ public class Userfinaldata : MonoBehaviour {
 		participantAge = participantage.participantAge;
 
 	}
+
+	public void SaveParticipantNumber()
+	{
+		participantNumberObject = GameObject.Find ("ParticipantNumberText");
+		participantNumberData = participantNumberObject.GetComponent<Capturetextdata> ();
+		participantNumber = participantNumberData.participantNumber;
+
+	}
+
 }
