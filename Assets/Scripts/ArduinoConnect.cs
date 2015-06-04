db602536744937d01b1b81280194d77a6930780c
hbcasper <hb.casper@gmail.com>
2015-06-05 01:08:25 +0200

    Some Advances in Arduino code

--- Assets/Scripts/ArduinoConnect.cs
+++ Assets/Scripts/ArduinoConnect.cs
@@ -1,32 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using System.IO.Ports; 
-using System.Threading;
+using System.IO.Ports; 
+
 //
 public class ArduinoConnect : MonoBehaviour {
-//
+
+
+
+	public static SerialPort sp = new SerialPort("COM1", 9600);
+	int Balanceresult=0;
 //
 //	private ArduinoInputBehavior BalanceVar;
 //	public GameObject balanceVar;
 //
 //	public bool rightSetting; // input of pixi compared to the instruction, effects the READY -Button
-//	public static SerialPort sp = new SerialPort("COMX//check here arduino connection", 9600);
 //	public static string strIn;
 //
 //	// Use this for initialization
-//	void Start () {
+void Start () {
+
+		sp.Open ();
+		sp.ReadTimeout= 1;
 //
-//		OpenConnection();
+		OpenConnection();
 //		balanceVar = GameObject.Find ("Scale");
 //		BalanceVar = balanceVar.GetComponent<ArduinoInputBehavior> (); 
 //
-//	}
+	}
 //	
 //	// Update is called once per frame
-//	void Update () {
+		void Update () {
+		if (sp.IsOpen) {
+			try {
+				MoveScale (sp.ReadByte ());
+				print (sp.ReadByte ());
+			} catch (System.Exception) {
+				throw;
+			}
+			
 //		strIn = sp.ReadLine();
 //		print(strIn);
-//	}
+		}
+	}
+
+		void MoveScale(int Direction){
+		if (Direction == 1) {
+			Debug.Log ("R");
+		}
+		if (Direction == 2) {
+			Debug.Log ("L");
+		}
+	}
+
+
 //
 //	void ReceiveInput (){
 //		// receive 4 bytes 1 = position 1;  2 = color 1; 3 = position 2; 4 = color 2
@@ -36,64 +62,54 @@ public class ArduinoConnect : MonoBehaviour {
 //	}
 //
 //
-//	void ScaleMove ()
-//	{
-//		if (BalanceVar.balance == 1) {
-//
-//			SendLeft(); 
-//			
-//		} else if (BalanceVar.balance == 2) {
-//
-//			SendRight();
-//			
-//		} else 
-//		{
-//			SendBalance(); 
-//		}
-//	}
-//
-//	void SendLeft(){
-//		sp.Write("AL");
-//	}
-//	void SendRight(){
-//		sp.Write ("AR"); 
-//	}
-//	void SendBalance(){
-//		sp.Write ("AH"); 
-//	}
-//
-//	public void OpenConnection() 
-//	{
-//		if (sp != null) 
-//		{
-//			if (sp.IsOpen) 
-//			{
-//				sp.Close();
-//				print("Closing port, because it was already open!");
-//			}
-//			else 
-//			{
-//				sp.Open();  // opens the connection
-//				sp.ReadTimeout = 50;  // sets the timeout value before reporting error
-//				print ("Port Opened!");
-//			}
-//		}
-//		else 
-//		{
-//			if (sp.IsOpen)
-//			{
-//				print("Port is already open");
-//			}
-//			else 
-//			{
-//				print("Port == null");
-//			}
-//		}
-//	}
-//	
-//	void OnApplicationQuit() 
-//	{
-//		sp.Close();
-//	}
-//
+	void ScaleMove ()
+	{
+		if (Balanceresult == 1) {
+
+			sp.Write("AL"); //why that values?????
+			
+		} else if (Balanceresult== 2) {
+
+			sp.Write ("AR");
+			
+		} else if (Balanceresult == 0)
+		{
+			sp.Write ("AH");  
+		}
+	}
+
+public void OpenConnection() 
+	{
+		if (sp != null) 
+		{
+			if (sp.IsOpen) 
+			{
+				sp.Close();
+				print("Closing port, because it was already open!");
+			}
+			else 
+			{
+				sp.Open();  // opens the connection
+				sp.ReadTimeout = 50;  // sets the timeout value before reporting error
+				print ("Port Opened!");
+			}
+		}
+		else 
+		{
+			if (sp.IsOpen)
+			{
+				print("Port is already open");
+			}
+			else 
+			{
+				print("Port == null");
+			}
+		}
+	}
+	
+	void OnApplicationQuit() 
+	{
+		sp.Close();
+	}
+
 }
