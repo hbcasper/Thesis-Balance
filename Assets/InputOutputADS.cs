e2e1513236d9cc4e67dd305c1ff9300608b1b5fe
hbcasper <hb.casper@gmail.com>
2015-06-05 15:58:29 +0200

    AD advances

--- Assets/InputOutputADS.cs
+++ Assets/InputOutputADS.cs
@@ -5,18 +5,37 @@ public class InputOutputADS : MonoBehaviour {
 
 	// Use this for initialization
 
+	int reactionTime;
+	int numberOfErrors;
+	int numberOfmovements;
+
+	int numberofcubes;
+	int numberofcolors;
+	int numberofplaces;
+
+
 
 	public void DeclareParameters () {
+		 //I have no idea 
 
 	}
 	
 	// Update is called once per frame
 	public void ReceiveInputsADS () {
+
 	// Read Inputs of the ADSystem
+
+		numberofcubes = 1; //equal to inputs
+		numberofcolors = 1;
+		numberofplaces = 1;
 	}
 
-	public void SendInputsADS(){
+	public void SendOutputADS(){
 	//Call the performance inputs and send them
+
+		reactionTime = 3;
+	    numberOfErrors = 2;
+		numberOfmovements = 5;
 
 		}
 
