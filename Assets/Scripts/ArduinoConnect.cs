8c09e9a5a2d8ed596819046f0a48ce56c6ad3cda
hbcasper <hb.casper@gmail.com>
2015-06-05 19:24:35 +0200

    Some advances in Arduino
    I'm able to open the port with a cable. that is all xD

--- Assets/Scripts/ArduinoConnect.cs
+++ Assets/Scripts/ArduinoConnect.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO.Ports; 
+using System.Threading;
 
 //
 public class ArduinoConnect : MonoBehaviour {
 
 
 
-	public static SerialPort sp = new SerialPort("COM1", 9600);
+	public static SerialPort sp = new SerialPort("COM3", 9600);
 	int Balanceresult = 0;
 //
 //	private ArduinoInputBehavior BalanceVar;
@@ -19,38 +20,43 @@ public class ArduinoConnect : MonoBehaviour {
 //	// Use this for initialization
 void Start () {
 
-		sp.Open ();
-		sp.ReadTimeout= 1;
+	//	sp.Open ();
+	//	sp.ReadTimeout= 1;
 //
+		gameObject.GetComponent<Renderer>().material.color = Color.black;
 		OpenConnection();
+
 //		balanceVar = GameObject.Find ("Scale");
 //		BalanceVar = balanceVar.GetComponent<ArduinoInputBehavior> (); 
 //
 	}
 //	
 //	// Update is called once per frame
-		void Update () {
-		if (sp.IsOpen) {
-			try {
-				MoveScale (sp.ReadByte ());
-				print (sp.ReadByte ());
-			} catch (System.Exception) {
-				throw;
-			}
+		public void Testarduino () {
+			gameObject.GetComponent<Renderer> ().material.color = Color.magenta;
+			sp.Write ("AR");
+
+//		if (sp.IsOpen) {
+//			try {
+//				MoveScale (sp.ReadByte ());
+//				print (sp.ReadByte ());
+//			} catch (System.Exception) {
+//				throw;
+//			}
 			
 //		strIn = sp.ReadLine();
 //		print(strIn);
 		}
-	}
 
-		void MoveScale(int Direction){
-		if (Direction == 1) {
-			Debug.Log ("R");
-		}
-		if (Direction == 2) {
-			Debug.Log ("L");
-		}
-	}
+
+//		void MoveScale(int Direction){
+//		if (Direction == 1) {
+//			Debug.Log ("R");
+//		}
+//		if (Direction == 2) {
+//			Debug.Log ("L");
+//		}
+//	}
 
 
 //
@@ -62,36 +68,40 @@ void Start () {
 //	}
 //
 //
-	void ScaleMove ()
-	{
-		if (Balanceresult == 1) {
+//	void ScaleMove ()
+//	{
+//		if (Balanceresult == 1) {
+//
+//			sp.Write("AL"); //why that values????? That's what Alex sends to the arduiono...
+//			
+//		} else if (Balanceresult== 2) {
+//
+//			sp.Write ("AR");
+//			
+//		} else if (Balanceresult == 0)
+//		{
+//			sp.Write ("AH");  
+//		}
+//	}
 
-			sp.Write("AL"); //why that values????? That's what Alex sends to the arduiono...
-			
-		} else if (Balanceresult== 2) {
-
-			sp.Write ("AR");
-			
-		} else if (Balanceresult == 0)
-		{
-			sp.Write ("AH");  
-		}
-	}
-
-	public void OpenConnection() 
-	{
+	public void OpenConnection(){
 		if (sp != null) 
 		{
 			if (sp.IsOpen) 
 			{
 				sp.Close();
+				sp.ReadTimeout = 50; 
 				print("Closing port, because it was already open!");
+				gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+
 			}
 			else 
 			{
 				sp.Open();  // opens the connection
 				sp.ReadTimeout = 50;  // sets the timeout value before reporting error
+				gameObject.GetComponent<Renderer>().material.color = Color.green;
 				print ("Port Opened!");
+
 			}
 		}
 		else 
@@ -99,10 +109,12 @@ void Start () {
 			if (sp.IsOpen)
 			{
 				print("Port is already open");
+				gameObject.GetComponent<Renderer>().material.color = Color.yellow;
 			}
 			else 
 			{
 				print("Port == null");
+				gameObject.GetComponent<Renderer>().material.color = Color.red;
 			}
 		}
 	}
@@ -112,4 +124,12 @@ void Start () {
 		sp.Close();
 	}
 
+	void Update (){
+		if (sp.IsOpen) {
+			//sp.ReadTimeout = 50;
+			Debug.Log ("Imhere");
+			//Debug.Log (sp.ReadByte ());
+		}
+	}
+	
 }
