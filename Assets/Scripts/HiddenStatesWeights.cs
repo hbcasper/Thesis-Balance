270898f790b362d34b2d007d82f173188e43a845
AnAdventurousCat <gianna.ballmann@gmail.com>
2015-06-09 13:44:55 +0200

    Pretty Little IndicatorWeights

---
+++ Assets/Scripts/HiddenStatesWeights.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenStatesWeights : MonoBehaviour {
+
+
+	private Renderer Weight; 
+	public GameObject CubeBelong; 
+	public Color cubeColor; 
+	// private Color clear = (0,0,0,0); 
+
+//	public Color red = Color.red;
+//	public Color yellow = Color.yellow; 
+
+	// Use this for initialization
+	void Start () {
+
+		//cubeColor = Color.blue; 
+		gameObject.GetComponent<Renderer> ().material.color = Color.clear;
+		ColorChange (); 
+		//Debug.Log ("Called Function");
+
+	}
+
+	void ColorChange(){
+	
+		if (cubeColor == Color.yellow)
+		{
+			gameObject.GetComponent<Renderer>().enabled=true;  
+			gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+			//Debug.Log("Should Be YEEEEEEEEEELLLLOW");
+
+		} 
+		else if (cubeColor == Color.red)
+		{
+			//gameObject.SetActive(true);
+			gameObject.GetComponent<Renderer>().enabled=true; 
+			gameObject.GetComponent<Renderer> ().material.color = Color.red;
+			//Debug.Log("Should be REEEEEEEEEEEEEED");
+		} 
+		else 
+		{	
+			gameObject.GetComponent<Renderer>().enabled=false;  
+			//gameObject.GetComponent<Renderer> ().material.color = Color.clear;
+//			Debug.Log("Invisible");
+			//gameObject.SetActive(false); 
+		}
+
+		//Debug.Log("Changed Color!!!!!!!!!!!!!!!! Or not...");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		cubeColor = CubeBelong.GetComponent<Renderer> ().material.color; 
+		ColorChange (); 
+		//Debug.Log ("Update Called");
+	
+	}
+}
--- Assets/Scripts/TwoWeights.cs
+++ Assets/Scripts/TwoWeights.cs
@@ -6,13 +6,28 @@ public class TwoWeights : MonoBehaviour {
 
 	//Sets the GameObjectActive only when the cube color is yellow 
 
+	public GameObject CubeBelong; 
+	public Color colorOfCube; 
+	// Seecubes otherScript = GetComponent<Seecubes>(); 
+
 	// Use this for initialization
 	void Start () {
-	
+		colorOfCube = CubeBelong.GetComponent<Renderer> ().material.color;
+		SetActive2Cube (); 
+		Debug.Log (colorOfCube.ToString ());
+	}
+
+	void SetActive2Cube(){
+
+		if (colorOfCube == Color.yellow) {
+			gameObject.SetActive(true); 
+			Debug.Log ("Should be active"); 
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		colorOfCube = CubeBelong.GetComponent<Renderer> ().material.color;
+		SetActive2Cube();
 	}
 }
