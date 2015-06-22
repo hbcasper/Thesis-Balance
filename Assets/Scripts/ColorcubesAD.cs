5464bf1dff3f7a9db76ce4898910c5cc0d058c70
hbcasper <hb.casper@gmail.com>
2015-06-22 12:37:05 +0200

    again

---
+++ Assets/Scripts/ColorcubesAD.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorcubesAD : MonoBehaviour {
+
+
+	Seecubes[] activecubes;
+	public int totalLeft = 0;
+	public int totalRight = 0;
+
+	public void Start(){
+		activecubes = gameObject.GetComponentsInChildren<Seecubes> ();
+	}
+
+
+	public void ActiveCubesAD() {
+		
+		foreach (Seecubes script in activecubes) {
+			
+			script.ActiveCubesAD ();
+
+
+
+		}
+		CalculateWeight();
+	}
+
+		public void DeactiveCubesAD() {
+		totalLeft = 0;
+		totalRight = 0;
+			
+			foreach (Seecubes script in activecubes) {
+				
+				script.NoActiveCubesAD();
+
+			}
+	}
+
+	public void CalculateWeight(){
+		foreach (Seecubes script in activecubes) {
+			
+			if (script.gameObject.tag == ("Left")){
+				totalLeft = totalLeft + script.myValue;
+			}
+			if (script.gameObject.tag == ("Right")){
+					totalRight = totalRight + script.myValue;
+			
+			
+		}
+
+		}
+
+}
+}
---
+++ Assets/Scripts/InputOutputADS.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class InputOutputADS : MonoBehaviour {
+
+
+	// Performance Parameters
+	public float reactionTime;
+	public float answercorrect;
+
+	// Difficulty Parameters
+	public int numberofcubes;
+	public int numberofcolors;
+	public float equalcolors;
+	public float equaldistance;
+	
+	//Calling values
+
+	private GameObject ExerciseManager;
+	private Textfile performdata;
+
+	private GameObject Scale;
+	private Animate Exercisedata;
+
+	void Start(){
+		ExerciseManager = GameObject.Find ("Exercisemanager");
+		performdata = ExerciseManager.GetComponent<Textfile> ();
+
+		Scale = GameObject.Find ("Scale");
+		Exercisedata = Scale.GetComponent<Animate> ();
+	}
+
+	public void DeclareParameters () {
+		 //I have no idea of how to
+		ReceiveInputsADS ();
+	}
+
+	public void ReceiveInputsADS () {
+
+	// Read Inputs of the ADSystem
+
+	// all the values should be from  0 to 1
+
+		numberofcubes = 1; //(0-1) 0 = 1 cube - 1 = 2 cubes
+		numberofcolors = 1; //(0- 1) 0 = 1 color - 1 = 2 colors
+		equalcolors = 1; //(0-1) 0 = Same colos en each side 1 = Different color in each side
+		equaldistance = 1; // ( 0 -1) 0 = Same distance on each side 1 = Different distance in each side
+
+		GameObject Instructions = GameObject.Find ("Instructions");
+		Instructions.GetComponent<Instruction> ().ADexercise ();
+
+	}
+
+	public void SendOutputADS(){
+
+		// all the values are from 0 - 1 
+
+		reactionTime = Convert.ToSingle(performdata.endTime.Second) /100; // NECESSARY to pass from 0 -1 
+	    answercorrect = Convert.ToSingle (Exercisedata.correct); // 0-1 0 = Incorrect. 1 = Correct
+	
+		ReceiveInputsADS ();
+
+		}
+
+
+}
