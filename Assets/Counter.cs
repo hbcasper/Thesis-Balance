52df7ad64e358020754208f88a61b831dec53e94
hbcasper <hb.casper@gmail.com>
2015-06-26 03:29:52 +0200

    +

---
+++ Assets/Counter.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System.Text;
+using System;
+
+public class Counter : MonoBehaviour {
+
+	DateTime startTime;
+	DateTime actualTime;
+
+	int maxtime;
+
+	public string counterMode;
+
+	public TimeSpan passedTime;
+
+	public GameObject Scale;
+	public GameObject Fulcrum;
+	public GameObject Choosebuttons;
+	public GameObject Next;
+	public GameObject Question;
+	public GameObject Correct;
+	public GameObject Incorrect;
+
+
+	Animate Scaledata;
+
+	Textfile Log;
+
+
+	void Start(){
+		counterMode = "none";
+
+	
+		Log = gameObject.GetComponent<Textfile> ();
+		Scaledata = Scale.GetComponent<Animate> ();
+
+
+		taskcounter();
+	}
+
+	// Use this for initialization
+	public void taskcounter () {
+
+		startTime = System.DateTime.Now;
+		maxtime = 4;
+
+		counterMode = "scale";
+	
+	}
+	void answercounter () {
+		
+		startTime = System.DateTime.Now;
+		maxtime = 3;
+		
+		counterMode = "ask";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		actualTime = System.DateTime.Now;
+
+		passedTime = actualTime.Subtract (startTime);
+
+		if (passedTime.Seconds > maxtime) {
+
+			if (counterMode == "scale"){
+				Debug.Log ("maxscaletime");
+				Scale.SetActive(false);
+				Fulcrum.SetActive(false);
+				Choosebuttons.SetActive(true);
+				answercounter();
+				Question.SetActive(true);
+
+			}
+			else if (counterMode == "ask"){
+				Debug.Log ("maxasktime");
+				Scale.SetActive(true);
+				Fulcrum.SetActive(true);
+				Next.SetActive(true);
+				Correct.SetActive(true);
+				Incorrect.SetActive(true);
+				Choosebuttons.SetActive(false);
+				Log.StoreEndTime();
+				Log.write();
+				Question.SetActive(false);
+				//Animate.animate();
+				counterMode = "none";
+
+			}
+		
+		}
+	
+	}
+}
---
+++ Assets/Showcounter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using UnityEngine.UI;
+
+
+public class Showcounter : MonoBehaviour {
+
+	Text counter;
+	int lefttime;
+
+	public GameObject ExerciseManager;
+	Counter TimeCounter;
+
+
+	// Use this for initialization
+	void Start () {
+		counter = GetComponent <Text> ();
+		TimeCounter = ExerciseManager.GetComponent<Counter> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (TimeCounter.counterMode == "scale"){
+			lefttime = 4-TimeCounter.passedTime.Seconds;
+
+		}
+		else if (TimeCounter.counterMode == "ask"){
+			lefttime = 3-TimeCounter.passedTime.Seconds;
+		
+		}
+
+		counter.text="00:0"+lefttime.ToString();
+	
+	}
+}
