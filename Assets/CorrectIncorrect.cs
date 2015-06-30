814b4dfa06944d7c93cbf5eea9b8ca1e25943442
hbcasper <hb.casper@gmail.com>
2015-06-30 04:26:32 +0200

    Ready

--- Assets/CorrectIncorrect.cs
+++ Assets/CorrectIncorrect.cs
@@ -7,31 +7,43 @@ public class CorrectIncorrect : MonoBehaviour {
 	private Animate Result;
 	GameObject Scale;
 	Text Resulttext;
+	public GameObject Audio;
+	PlaySounds AudioFeedback;
 	
 	// Use this for initialization
 	void Start () {
 		Scale = GameObject.Find ("Scale");
 		Result = Scale.GetComponent<Animate> ();
 		Resulttext = GetComponent<Text> ();
+
 		
 		Result = Scale.GetComponent<Animate> ();
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
 		
 		if (Result.correct == 0)
 		{ 
 			Resulttext.color = Color.red; 
 			Resulttext.text = "Incorrect";
+		
+
 		}
 		
 		if (Result.correct == 1)
 		{
 			Resulttext.color = Color.green; 
 			Resulttext.text = "Correct";
+		
 		}
 		
+
 	}
+
+
+
+
+
 }
